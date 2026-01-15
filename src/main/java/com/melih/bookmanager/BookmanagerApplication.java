f86491d559a2f5bf2e47060ce1d6d188a4af3258
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-01-15 20:08:36 +0100

    finished dummy method for later testing purposes

--- src/main/java/com/melih/bookmanager/BookmanagerApplication.java
+++ src/main/java/com/melih/bookmanager/BookmanagerApplication.java
@@ -3,11 +3,14 @@ package com.melih.bookmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static models.Book.generateDummyBooks;
+
 @SpringBootApplication
 public class BookmanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookmanagerApplication.class, args);
+		System.out.println(generateDummyBooks());
 	}
 
 }
--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -2,6 +2,7 @@ package models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Book {
     private String ISBN;
@@ -17,38 +18,16 @@ public class Book {
     }
 
     // Getters
-    public String getISBN() {
-        return ISBN;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getPages() {
-        return pages;
-    }
+    public String getISBN() {return ISBN;}
+    public String getTitle() {return title;}
+    public String getAuthor() {return author;}
+    public int getPages() {return pages;}
 
     // Setters
-    public void setISBN(String ISBN) {
-        this.ISBN = ISBN;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
+    public void setISBN(String ISBN) {this.ISBN = ISBN;}
+    public void setTitle(String title) {this.title = title;}
+    public void setAuthor(String author) {this.author = author;}
+    public void setPages(int pages) {this.pages = pages;}
 
     // For a quick overview
     public String toString() {
@@ -56,7 +35,7 @@ public class Book {
     }
 
     // Generate a list of Dummy-Books for testing purposes
-    public List<Book> generateDummyBooks() {
+    public static List<Book> generateDummyBooks() {
         List<Book> books = new ArrayList<Book>();
         String[] ISBNs = {
                 "978-3-16-148410-0",
@@ -70,6 +49,36 @@ public class Book {
                 "978-0-14-044926-6",
                 "978-3-86680-192-9"
         };
+        String[] titles = {
+                "Der Wind am Ende der Welt",
+                "Das Schweigen der alten Eiche",
+                "Hafen der verlorenen Träume",
+                "Die mechanische Rose: Ein Steampunk-Abenteuer",
+                "Letzter Halt: Wien Hauptbahnhof",
+                "Sterne über Island",
+                "Der Schatten des Uhrmachers",
+                "Die vergessene Bibliothek von Palermo",
+                "Jenseits des Horizonts wartet das Licht",
+                "Bittersüße Mandeln"
+        };
+        String[] authors = {
+                "Franz Kafka",
+                "Hermann Hesse",
+                "Haruki Murakami",
+                "Albert Camus",
+                "Friedrich Nietzsche"
+        };
+        int[] pages = {300, 350, 402, 508, 117, 958};
+
+        for (int i = 0; i < titles.length; i++) {
+            String randomISBN = ISBNs[ThreadLocalRandom.current().nextInt(ISBNs.length)];
+            String randomTitle = titles[ThreadLocalRandom.current().nextInt(titles.length)];
+            String randomAuthor = authors[ThreadLocalRandom.current().nextInt(authors.length)];
+            int randomPages = pages[ThreadLocalRandom.current().nextInt(pages.length)];
+
+            Book book = new Book(randomISBN, randomTitle, randomAuthor, randomPages);
+            books.add(book);
+        }
         return books;
     }
 }
