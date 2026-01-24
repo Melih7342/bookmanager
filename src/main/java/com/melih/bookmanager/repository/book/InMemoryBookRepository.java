5ef7e2791bd73d46adb236db9bb36d065b521f7c
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-01-24 23:03:04 +0100

    added basic structure of UserService

--- src/main/java/com/melih/bookmanager/repository/book/InMemoryBookRepository.java
+++ src/main/java/com/melih/bookmanager/repository/book/InMemoryBookRepository.java
@@ -2,12 +2,21 @@ package com.melih.bookmanager.repository.book;
 
 import com.melih.bookmanager.api.model.Book;
 import org.springframework.stereotype.Repository;
+import static com.melih.bookmanager.service.BookService.generateDummyBooks;
 
 import java.util.*;
 
 @Repository
 public class InMemoryBookRepository implements BookRepository {
     private Map<String, Book> books = new HashMap<>();
+
+    // For testing
+    public InMemoryBookRepository() {
+        List<Book> dummyBooks = generateDummyBooks();
+        for(Book book : dummyBooks) {
+            this.books.put(book.getISBN(), book);
+        }
+    }
 
     @Override
     public List<Book> findAll() {
--- src/main/java/com/melih/bookmanager/service/UserService.java
+++ src/main/java/com/melih/bookmanager/service/UserService.java
@@ -1,14 +1,33 @@
 package com.melih.bookmanager.service;
 
+import com.melih.bookmanager.api.model.User;
 import com.melih.bookmanager.repository.user.InMemoryUserRepository;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Service
-@AllArgsConstructor
 public class UserService {
     private final InMemoryUserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public void register(String username, String password) {}
+
+    public void login(String username, String password) {}
+
+    public void logout(String username) {}
+
+    public void deleteAccount(String username, String password) {}
+
+    public void changePassword(String username, String oldPassword, String newPassword) {}
+
 
 }
